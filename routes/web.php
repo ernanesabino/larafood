d98b0fbc02547290e86af3ce78ffe36817afe886
Ernane <ernanesabino@outlook.com>
2020-08-21 20:43:33 -0300

    Pesquisar plano

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::any('admin/plans/search', 'Admin\PlanController@search')->name('plans.search');
 Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plans.destroy');
 Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
 Route::post('admin/plans', 'Admin\PlanController@store')->name('plans.store');
